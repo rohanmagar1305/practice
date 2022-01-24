90abd16524c25f4b53648beecda818fb4fee2e73
rohanmagar1305 <97390806+rohanmagar1305@users.noreply.github.com>
2022-01-24 22:28:33 +0530

    changes in code

--- src/main/java/Facebook/Facebook/App.java
+++ src/main/java/Facebook/Facebook/App.java
@@ -1,13 +1,10 @@
 package Facebook.Facebook;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("Rohan");
     }
 }
